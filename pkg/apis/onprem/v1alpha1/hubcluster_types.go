dd867fdffa53ae90d307ecc96b9e681975aea045
Sohan Kunkerkar <sohank2602@gmail.com>
2019-10-08 17:11:04 -0400

    Add a new API type called "HubCluster" per the design document

---
+++ pkg/apis/onprem/v1alpha1/hubcluster_types.go
@@ -0,0 +1,50 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// HubClusterSpec defines the desired state of HubCluster
+// +k8s:openapi-gen=true
+type HubClusterSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// HubClusterStatus defines the observed state of HubCluster
+// +k8s:openapi-gen=true
+type HubClusterStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// HubCluster is the Schema for the hubclusters API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type HubCluster struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HubClusterSpec   `json:"spec,omitempty"`
+	Status HubClusterStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// HubClusterList contains a list of HubCluster
+type HubClusterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []HubCluster `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&HubCluster{}, &HubClusterList{})
+}
